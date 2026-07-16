a957654228b46c7809d70d114b6202630a042f71
highlander <dimand18062004h@gmail.com>
2026-07-16 08:16:06 +0500

    Merge remote-tracking branch 'origin/main'

--- pages/mapFuelPage.py
+++ pages/mapFuelPage.py
@@ -5,6 +5,6 @@ import flet_webview as ftwv
 def view(page: ft.Page):
 
     return ftwv.WebView(
-        url="https://toplivo.tbank.ru/?utm_source=autoru&utm_medium=bz_post",
+        url="https://gdebenz.ru/",
         expand=True,
     )
