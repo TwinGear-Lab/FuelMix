ce3916eeecddb22624b24f8c71ab67e6e58d24c1
highlander <dimand18062004h@gmail.com>
2026-07-16 10:27:19 +0500

    preRel v2.0

--- build/flutter/android/app/src/main/kotlin/com/flet/fuelmix/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.flet.fuelmix
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- pages/mapFuelPage.py
+++ pages/mapFuelPage.py
@@ -5,6 +5,7 @@ import flet_webview as ftwv
 def view(page: ft.Page):
 
     return ftwv.WebView(
-        url="https://gdebenz.ru/",
+        #https://gdebenz.ru/
+        url="https://yandex.ru/maps/50/perm/search/азс/?ll=56.229441%2C58.010454&sll=56.229441%2C58.010454&sspn=0.310020%2C0.110610&z=12",
         expand=True,
     )
